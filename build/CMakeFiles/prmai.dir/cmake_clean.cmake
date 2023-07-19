671f128a8d7354b02838a2b28d90b7661b85f710
flutterdro <dimanchikbest@icloud.com>
2023-07-19 18:07:53 +0300

    start

---
+++ build/CMakeFiles/prmai.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/prmai.dir/src/main.cpp.o"
+  "CMakeFiles/prmai.dir/src/main.cpp.o.d"
+  "prmai"
+  "prmai.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/prmai.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
