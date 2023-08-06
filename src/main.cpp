93b677f656d1bf4e2be57d183ff94887a1d0971c
flutterdro <dimanchikbest@icloud.com>
2023-08-06 09:04:05 +0300

    implemented a simple decision tree clasifier

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,73 @@
 #include <fmt/core.h>
+#include <fstream>
+#include <sstream>
+#include <string>
 #include "rtw/matop.h"
+#include "rtw/dectree.h"
 
 using namespace rtw;
 
+enum class Iris {
+    Iris_Setosa = 1,
+    Iris_Versicolor = 2,
+    Iris_Virginica = 3,
+};
+
 int main() {
-    Matrix mat3;
-    {float a[]{
-        1, 0, 0, 8,
-        0, 1, 3, 7
-    };
-    float b[]{
-        2, 1,
-        4, 0,
-        2, 5
-    };
-    float c[]{
-        1, 1, 1, 1,
-        1, 1, 1, 1
-    };
-    Matrix mat1(a, 4, 2); Matrix mat2(b, 2, 3); Matrix mat4(c, 4, 2);
-    mat3 = 2.0f * mat2 * (mat1 + mat4);}
-    mat3.print();
+    std::vector<std::vector<float>> data;
+    if (std::ifstream fin{"../iris/train.data"}) {
+        std::string line;
+        while (getline(fin, line)) {
+            data.push_back(std::vector<float>{});
+            std::istringstream ssin(line);
+            float buff = 0;
+            while (ssin >> buff) {
+                data.back().push_back(buff);
+            }
+        }
+    } else {
+        fmt::print("failed to open file");
+    }
+
+    DecisionTreeClasifier dtree(data, 4, 0, 0);
+
+    std::vector<std::vector<float>> test;
+    if (std::ifstream fin{"../iris/test.data"}) {
+        std::string line;
+        while (getline(fin, line)) {
+            test.push_back(std::vector<float>{});
+            std::istringstream ssin(line);
+            float buff = 0;
+            while (ssin >> buff) {
+                test.back().push_back(buff);
+            }
+        }
+    } else {
+        fmt::print("failed to open file");
+    }
+    float count_right{0};
+    float count{0};
+    for (const auto& tes : test) {
+        float expected = tes.back();
+        float predicted = dtree.classify(tes);
+        fmt::print("expected: {}, predicted: {} | ", expected, predicted);
+        if (expected == predicted) {
+            ++count_right;
+            fmt::print("correct\n");
+        } else {
+            fmt::print("wrong\n");
+        }
+        ++count;
+    }
+    fmt::print("------------");
+    fmt::print("total accuracy: {}%\n", count_right/count * 100);
+    
+    // for (auto dat : data) {
+    //     for (auto da : dat) {
+    //         fmt::print("{} ", da);
+    //     }
+    //     fmt::print("\n");
+    // }
+    //fmt::print("{}\n", rtw::gini(data));
     fmt::print("Hello world\n");
 }
---
+++ src/rtw/dectree.h
@@ -0,0 +1,61 @@
+#ifndef _DECTREE_H_
+#define _DECTREE_H_
+
+#include <vector>
+#include <map>
+#include <algorithm>
+#include <numeric>
+#include <span>
+
+#include <fmt/format.h>
+
+namespace rtw {
+enum class NodeType {
+    Branch,
+    Leaf
+};
+namespace {
+class Node {
+public:
+    Node() : left(nullptr), right(nullptr), parametr(std::make_pair(NAN, -1)) {}
+    Node(Node* left, Node* right, float split, int feature_index) 
+        : left(left), right(right), parametr(std::make_pair(split, feature_index)) {}
+    Node(float label) : left(nullptr), right(nullptr), parametr(std::make_pair(label, -1)) {}
+
+    Node* left;
+    Node* right;
+    std::pair<float, int> parametr;
+};
+}
+
+class DecisionTreeClasifier {
+public:
+    DecisionTreeClasifier(std::vector<std::vector<float>>&,
+                          int max_depth, int splitter, int min_smaple_split);
+    DecisionTreeClasifier()                                   = delete;
+    DecisionTreeClasifier(const DecisionTreeClasifier& other) = delete;
+    DecisionTreeClasifier(DecisionTreeClasifier&& other)      = delete;
+    ~DecisionTreeClasifier();
+    
+
+    [[nodiscard]]float classify(const std::vector<float>&) const noexcept;
+
+private:
+    [[nodiscard]]std::optional<std::pair<float, int>> 
+    bestSplit(const std::vector<std::span<float>>&) const noexcept;
+
+    void classifyImpl(const std::vector<float>&, Node*, float&) const noexcept;
+
+    void destroyTree(Node*);
+    [[nodiscard]]Node* buildTree(std::vector<std::span<float>>&, int);
+private:
+    Node* m_root;
+    const int max_depth;
+};
+
+float gini(const std::vector<std::span<float>>&);
+
+
+}
+
+#endif
